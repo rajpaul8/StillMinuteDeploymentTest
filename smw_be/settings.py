13e0862b555b551613aaae34da36c51ca024dc28
rajpaul8 <rajpaul83@gmail.com>
2022-12-09 02:04:41 +0530

    Admin Template fix

--- smw_be/settings.py
+++ smw_be/settings.py
@@ -47,7 +47,7 @@ ROOT_URLCONF = 'smw_be.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'smw_ui/build' )],
+        'DIRS': [os.path.join(BASE_DIR, 'smw_ui/build' ), os.path.join(BASE_DIR, 'templates'),],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
